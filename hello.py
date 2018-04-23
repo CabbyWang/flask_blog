a89ddd26b73b44c0183d6e4b8f29a9141398af88
wangsiyong <18489969713@163.com>
2018-04-23 19:20:50 +0800

    模板渲染

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import template_rendered
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -7,13 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    # return '<h1> Hello {name}</h1>'.format(name=name)
-    return '<h1> Hello {name}</h1>'.format(**{'name': name})
+    # return render_template('user.html', **{'name': name})
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
