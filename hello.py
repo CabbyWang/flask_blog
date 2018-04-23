993b5018f33996d863692b934da7ae887af0c5a8
wangsiyong <18489969713@163.com>
2018-04-23 19:13:56 +0800

    动态路由

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import template_rendered
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    # return '<h1> Hello {name}</h1>'.format(name=name)
+    return '<h1> Hello {name}</h1>'.format(**{'name': name})
 
 
 if __name__ == '__main__':
